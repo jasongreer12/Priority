01edafc04d2fd439adb5aec92689c68d93cc6388
Alex-Seda <alexseda04@outlook.com>
2025-02-22 18:20:08 -0600

    Pseudocode for ItemViewModel

---
+++ Priority/ViewModels/ItemViewModel.swift
@@ -0,0 +1,60 @@
+//
+//  ItemViewModel.swift
+//  Priority
+//
+//  Created by Alex on 2/22/25.
+//
+
+import Foundation
+import SwiftUI
+
+/*
+
+class ItemViewModel: ObservableObject{
+ @Published var items: [item] = []
+ 
+ 
+ // What to do when the app loads up
+ init() {
+    loadItems()
+ }
+ 
+ 
+ // Store notes in database
+ private func storeNotes(){}
+ 
+ 
+ // Retrieve notes from database
+ private func loadNotes(){}
+ 
+ 
+ // Add an item to the list
+ func addItem(){}
+ 
+ 
+ // Toggle the "completed" field of an item
+ func toggleCompletion(){}
+ 
+ 
+ // Delete an item
+ func deleteItem(){}
+ 
+ 
+ // Delete multiple items
+ func deleteTheseItems(Item1, Item2, Item3, ... ){}
+ 
+ 
+ // Edit any number of fields of an item
+ func editItem(OptionalField-1, OptionalField-2, ... ){
+    The code in here will only edit the fields you input
+    All other fields will have a default value of null, and the code in here will not touch them
+ }
+ 
+ 
+ // Returns the item with the given identifier if it is in the "items" list
+ func getItem(Item ID) -> Item? {
+    Iterates through "items" to find the item with the identifier given
+ }
+}
+ 
+*/
