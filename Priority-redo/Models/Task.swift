13f88d4488ca7f0cc70696bd47c7e787b20286b3
JManB055 <jmwyckoff8@gmail.com>
2025-04-10 15:34:35 -0500

    Still Working

---
+++ Priority-redo/Models/Task.swift
@@ -0,0 +1,8 @@
+//
+//  Task.swift
+//  Priority-redo
+//
+//  Created by Joshua Wyckoff on 4/10/25.
+//
+
+import Foundation
---
+++ Priority-redo/Models/TaskManager.swift
@@ -0,0 +1,8 @@
+//
+//  TaskManager.swift
+//  Priority-redo
+//
+//  Created by Joshua Wyckoff on 4/10/25.
+//
+
+import Foundation
