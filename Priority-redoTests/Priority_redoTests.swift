162a00782f5350da349c15e4e262667691d56b55
Erin <erindunlap37@gmail.com>
2025-04-11 11:18:02 -0500

    added unit tests for main funtionality

---
+++ Priority-redoTests/Priority_redoTests.swift
@@ -0,0 +1,16 @@
+//
+//  Priority_redoTests.swift
+//  Priority-redoTests
+//
+//  Created by Erin Dunlap on 4/11/25.
+//
+
+import Testing
+
+struct Priority_redoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
